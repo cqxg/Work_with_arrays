c4db0a257c5bc25d76adc8328eb5037457610ec7
cqxg <cqxg@yandex.ru>
2019-12-18 17:55:18 +0300

    feat: input.txt getting

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const App = () => {
+    const go = document.querySelector('.go');
 
+    const reqest = () => {
+        fetch('../input.txt')
+            .then(res => res.text())
+            .then(response => {
+                console.log(response);
+            })
+            .catch(err => console.log(err));
+    };
 
-
-
-}
+    go.addEventListener('click', reqest);
+};
 
 document.addEventListener('DOMContentLoaded', App);
