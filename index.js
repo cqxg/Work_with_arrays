d863586000d5f4ac34ff432244dd7e4c47523774
cqxg <cqxg@yandex.ru>
2019-12-29 18:37:20 +0300

    feat: add fill tool

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ const App = () => {
     const newRules = [];
     let arrOfArr = [];
 
+    const TOOLBOX = {
+        EMPTY_SPACE: ' ',
+        HOR_LINE: '-',
+        VER_LINE: '|',
+        FILL_LINE: 'x',
+    }
+
 
     // ---------------------------------------- UTILS--------------------------------------------------------------
     const request = (data, parse) => {
@@ -45,37 +52,53 @@ const App = () => {
             .catch(err => console.log(err));
     };
 
-    const makeNums = (y, x) => ({
+    const makeNums = ({ x, y }) => ({
         num_x: Number(x),
         num_y: Number(y),
     });
 
-    const inRange = (y, x) => {
-        const { num_x, num_y } = makeNums(y, x);
-
-        if (num_y < 0 || num_y > arrOfArr.length) {
+    const inRange = (x, y) => {
+        if (x < 0 || x > arrOfArr.length) {
             return false;
-        } else if (num_x < 0 || num_x > arrOfArr[0].length) {
+        } else if (y < 0 || y > arrOfArr[0].length) {
             return false;
         } else {
             return true;
         }
-    };
+    }
 
-    const border = (y, x) => {
-        const { num_x, num_y } = makeNums(y, x);
+    const border = ({ x, y }) => {
         const rangedArray = [
-            [num_x - 1, num_y - 1],
-            [num_x, num_y],
-            [num_x - 1, num_y + 1],
-            [num_x, num_y + 1],
-            [num_x, num_y - 1],
-            [num_x + 1, num_y],
-            [num_x + 1, num_y + 1],
-            [num_x + 1, num_y - 1]
+            [x - 1, y - 1],
+            [x, y],
+            [x - 1, y + 1],
+            [x, y + 1],
+            [x, y - 1],
+            [x + 1, y],
+            [x + 1, y + 1],
+            [x + 1, y - 1]
         ];
 
-        return rangedArray.filter(([y, x]) => inRange(y, x, console.log('координаты вокруг указанной:',y,x), console.log('......................')));
+        // const rangedArray = [
+        //     [num_y - 1, num_x - 1],
+        //     [num_y, num_x],
+        //     [num_y + 1, num_x - 1],
+        //     [num_y + 1, num_x],
+        //     [num_y - 1, num_x],
+        //     [num_y, num_x + 1],
+        //     [num_y + 1, num_x + 1],
+        //     [num_y - 1, num_x + 1]
+        // ];
+
+        console.log('Координаты внутри border:', x, y);
+        console.log('......................');
+        console.log('RangedArray: ', rangedArray);
+
+        const filteredArray = rangedArray.filter(([x, y]) => inRange({ x, y }));
+
+        console.log('FilteredArray: ', filteredArray);
+
+        return filteredArray;
     };
     // ------------------------------------------------------------------------------------------------------------
 
@@ -110,7 +133,7 @@ const App = () => {
     const parseInput = (response) => {
         const splitResponse = response.split('\n');
         const parsedResponse = splitResponse.map(command => command.split(" "));
-        
+
         parsedResponse.forEach(element => createRules(element));
         input_content.innerText = response;
     };
@@ -144,10 +167,12 @@ const App = () => {
     };
 
     const drawCanvas = ({ w, h }) => {
-        const closeLine = '-'.repeat(w).split('');
+        const { HOR_LINE, VER_LINE, EMPTY_SPACE } = TOOLBOX;
+
+        const closeLine = HOR_LINE.repeat(w).split('');
         const contentLine = [];
         for (let i = 0; i <= h - 1; i++) {
-            const tempLine = ('|' + ' '.repeat(w - 2) + '|').split('');
+            const tempLine = (VER_LINE + EMPTY_SPACE.repeat(w - 2) + VER_LINE).split('');
             contentLine.push(tempLine);
         };
 
@@ -161,42 +186,53 @@ const App = () => {
     };
 
     const drawLine = ({ x1, y1, x2, y2 }) => {
+        const { FILL_LINE } = TOOLBOX;
+
         if (x1 === x2) {
             for (let i = y1; i <= y2; i++) {
-                arrOfArr[Number(i)].fill('x', x1 - 1, x1);
+                arrOfArr[Number(i)].fill(FILL_LINE, x1 - 1, x1);
             }
         }
         else if (x1 !== x2) {
-            arrOfArr[Number(y1)].fill('x', x1 - 1, x2);
+            arrOfArr[Number(y1)].fill(FILL_LINE, x1 - 1, x2);
         };
     };
 
     const drawRect = ({ x_top, y_top, x_bottom, y_bottom }) => {
-        arrOfArr[Number(y_bottom)].fill('x', x_top - 1, x_bottom);
-        arrOfArr[Number(y_top)].fill('x', x_top - 1, x_bottom);
+        const { FILL_LINE } = TOOLBOX;
+
+        arrOfArr[Number(y_bottom)].fill(FILL_LINE, x_top - 1, x_bottom);
+
+        arrOfArr[Number(y_top)].fill(FILL_LINE, x_top - 1, x_bottom);
 
         for (let i = y_top; i <= y_bottom; i++) {
-            arrOfArr[Number(i)].fill('x', x_top - 1, x_top);
+            arrOfArr[Number(i)].fill(FILL_LINE, x_top - 1, x_top);
         };
 
         for (let j = y_top; j <= y_bottom; j++) {
-            arrOfArr[Number(j)].fill('x', x_bottom - 1, x_bottom);
+            arrOfArr[Number(j)].fill(FILL_LINE, x_bottom - 1, x_bottom);
         };
     };
 
-    const fillColour = ({ y, x }) => {
-        const { num_x, num_y } = makeNums(y, x);
+    const fillColour = ({ x, y, color }) => {
+        const { EMPTY_SPACE } = TOOLBOX;
 
-        if (arrOfArr[num_y][num_x] == ' ') {
+        const { num_x, num_y } = makeNums({ x, y });
+
+        console.log(arrOfArr[1][1])
+
+        console.log(`Координаты ${x}:${num_x}, ${y}:${num_y}`);
+
+        if (arrOfArr[num_y][num_x] == EMPTY_SPACE) {
             arrOfArr[num_y][num_x] = 'o';
 
-            const result = border(num_y, num_x);
+            const result = border({ x: num_x, y: num_y });
 
-            console.log(`Тут должен был быть заполненый массив вокруг координат ${y}, ${x}`, result);
+            console.log(`Тут должен был быть заполненый массив вокруг координат ${x}, ${y}`, result);
             console.log('................................');
             console.log('Сам массив arrOfArr:', arrOfArr);
 
-            result.forEach(([y, x]) => fillColour(y, x));
+            result.forEach(([x, y]) => fillColour({ x, y, color }));
         };
     };
     // --------------------------------------------------------------------------------------------------
